59ff79a4891b3c95ae6bd05da16210b591a887bb
Agata Kowal <bereza.agata@gmail.com>
2022-01-07 14:48:49 +0100

    Initial commit

---
+++ src/test/java/com/agata/learning/SpringGuru/SpringGuruApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agata.learning.SpringGuru;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringGuruApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
